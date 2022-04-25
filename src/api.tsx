ca844769efdd1033067b521bffa951d1851f8c64
Hugh Desmond <hughdesmond2006@yahoo.ie>
2022-04-25 20:36:24 +0100

    bug fix

--- src/api.tsx
+++ src/api.tsx
@@ -1,14 +1,11 @@
 import axios from "axios";
-import { infoData, listData } from "./testData";
 
 export const getCoinList = async () => {
-  return listData;
   const res = await axios.get(`${process.env.REACT_APP_API_URL!}/coinList`);
   return res.data.data;
 };
 
 export const getCoinInfo = async (symbol) => {
-  return infoData;
   const res = await axios.get(`${process.env.REACT_APP_API_URL!}/coinInfo`, {
     params: { symbol },
   });
